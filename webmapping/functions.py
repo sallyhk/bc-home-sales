3b75bfa51f7451cba61e4d3c5de1b8f9f19a00c0
sallykim515 <sallykim515@gmail.com>
2020-05-04 13:51:44 -0700

    Add # Beds, Baths info on web map pin

--- webmapping/functions.py
+++ webmapping/functions.py
@@ -2,24 +2,27 @@ import numpy
 
 
 def calc_thresholds(data):
-    l1_threshold = numpy.percentile(data, 100/3)
-    l2_threshold = numpy.percentile(data, 200/3)
+    l1_threshold = numpy.percentile(data, 100 / 3)
+    l2_threshold = numpy.percentile(data, 200 / 3)
 
     return l1_threshold, l2_threshold
 
 
-def make_html(ps, ar, pr, lnk, a):
+def make_html(row):
     html1 = """
-    <font size="2"><b> $ %s / sq<sup>2</sup> </b></font>
+    <font size="1.8"><b> $ %s / sq<sup>2</sup> </b></font>
     <br><font size="1.5">Total Area: %s sq<sup>2</sup></font>
     <br><font size="1.5">Price: %s </font>
+    <br><font size="1.5"> %s Beds, %s Baths </font>
     <br><font size="1.5"><a href= """
 
     html2 = """
     > %s </a></font>
     """
 
-    return html1 % (str(int(ps)), str(ar), pr) + "\"https://www." + lnk + "\"" + html2 % a
+    return html1 % (str(int(row["$/sq"])), str(row["Area"]), row["Price"],
+                    str(row["Bed"]), str(row["Bath"])) + "\"https://www." \
+           + row["Link"] + "\"" + html2 % row["Address"]
 
 
 def color_producer(psq, l1_threshold, l2_threshold):
