f3d4a40b600959c464749fc6809081a6a80a7069
sallykim515 <sallykim515@gmail.com>
2020-05-04 13:56:22 -0700

    Add multi-city capability & include bed/bath info

--- scraping/scraper.py
+++ scraping/scraper.py
@@ -7,41 +7,51 @@ from scraping.functions import ask_for_criteria
 
 
 def run(filepath="rew_data.csv"):
-    # input parameters
-    criteria = ask_for_criteria()
-
     # initialize
     try:
         os.remove(filepath)
     except:
         pass
 
-    last_pg_num = SalesPage(1, criteria).find_last_pg()
+    # gather all input values first
+    n = int(input("How many cities to search for? "))
+    criteria_list = []
+    for city_num in range(n):
+        criteria_list.append(ask_for_criteria(city_num+1))
 
-    # loop through each page
-    for pg_num in range(1, last_pg_num+1):
-        page = SalesPage(pg_num, criteria).page
+    # loop through each city
+    for i in range(len(criteria_list)):
+        criteria = criteria_list[i]
 
-        # for each posting, fetch information of interest
-        l = []
-        for posting in page:
-            h = HomeInfo(posting)
-            d = {"Address": h.address,
-                 "Latitude": h.latitude,
-                 "Longitude": h.longitude,
-                 "Price": h.price_str,
-                 "$/sq": h.dollar_sqft,
-                 "Area": h.area,
-                 "Link": h.link}
-            l.append(d)
+        print(f"\nScraping for City #{str(i+1)} {criteria} criteria...")
+        last_pg_num = SalesPage(1, criteria).find_last_pg()
 
-        print(f"{str(pg_num)} out of {str(last_pg_num)} page(s) done!")
-        time.sleep(2)
-        df = pandas.DataFrame(l)
+        # loop through each page
+        for pg_num in range(1, last_pg_num+1):
+            page = SalesPage(pg_num, criteria).page
 
-        # save each page at a time
-        if pg_num == 1:
-            df.to_csv(filepath)
-        else:
-            df.to_csv(filepath, mode='a', header=False)
+            # for each posting, fetch information of interest
+            l = []
+            for posting in page:
+                h = HomeInfo(posting)
+                d = {"Address": h.address,
+                     "Latitude": h.latitude,
+                     "Longitude": h.longitude,
+                     "Price": h.price_str,
+                     "$/sq": h.dollar_sqft,
+                     "Area": h.area,
+                     "Bed": h.bed,
+                     "Bath": h.bath,
+                     "Link": h.link}
+                l.append(d)
+
+            print(f"{str(pg_num)} out of {str(last_pg_num)} page(s) done!")
+            time.sleep(2)
+            df = pandas.DataFrame(l)
+
+            # save each page at a time
+            if i == 0 and pg_num == 1:
+                df.to_csv(filepath, header=True)
+            else:
+                df.to_csv(filepath, mode='a', header=False)
 
